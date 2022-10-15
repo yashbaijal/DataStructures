8342000417994adccd0e6f867327d6335b0aae67
yashbaijal <110350499+yashbaijal@users.noreply.github.com>
2022-10-15 15:53:37 +0530

    Update LengthofthelongestsubstringwithoutrepeatingCharactersA_1.java

--- Strings/LengthofthelongestsubstringwithoutrepeatingCharactersA_1.java
+++ Strings/LengthofthelongestsubstringwithoutrepeatingCharactersA_1.java
@@ -70,3 +70,53 @@ class Codechef
         System.out.println(res);
         System.out.println("longestUniqueSubString : "+str.substring(start,end+1));
     }
+==================================================================
+	
+	public static void longestUniqueSubString_2(String str) // TC=O(N) SC=O(N)
+    {
+        int start=0;
+        int maxlen=Integer.MIN_VALUE;
+        int low=0,high=0;
+        
+        HashMap<Character,Integer> map=new HashMap<>();
+        
+        for(int end=0;end<str.length();end++)
+        {
+            if(map.containsKey(str.charAt(end)))
+            {
+                start=Math.max(start,map.get(str.charAt(end))+1);
+            }
+            map.put(str.charAt(end),end);
+            if((end-start+1)>maxlen)
+            {
+                low=start;
+                high=end;
+            }
+            maxlen=Math.max(maxlen,end-start+1);
+        }
+        System.out.println("longestUniqueSubString : "+maxlen+" " +str.substring(low,high+1));
+    }
+=====================================================================
+// TC of this approach O(n) cuz (Note: Even though we have nested for loops, start only goes from 0 to s.length() once
+	public static void longestUniqueSubString(String s)
+    {
+        int start=0;
+        int maxlen=Integer.MIN_VALUE;
+        
+        int [] count=new int[128];
+        
+        for(int i=0;i<s.length();i++)
+        {
+            count[s.charAt(i)]++;
+            if(count[s.charAt(i)]>1)
+            {
+                while(count[s.charAt(i)]>1)
+                {
+                    count[s.charAt(start)]--;
+                    start++;
+                }
+            }
+            maxlen=Math.max(maxlen,(i-start+1));
+        }
+        System.out.println(maxlen);
+    }
