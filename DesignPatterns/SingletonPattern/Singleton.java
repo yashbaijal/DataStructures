bc443eed77c2287452fe22e62fbbf2b516e8fd6b
yashbaijal <110350499+yashbaijal@users.noreply.github.com>
2023-05-21 17:03:15 +0530

    Update Singleton.java

--- DesignPatterns/SingletonPattern/Singleton.java
+++ DesignPatterns/SingletonPattern/Singleton.java
@@ -2,7 +2,7 @@ package conceptsJava.SingleTonPattern;
 
 public class Singleton {
 
-    private static volatile Singleton instance=null;
+    private static volatile Singleton instance=null; // making volatile will insure no metod call will face half initialized objects.
     private int id;
 
     private Singleton(int id)
@@ -10,10 +10,10 @@ public class Singleton {
         this.id=id;
     }
 
-    public static Singleton getInstance(int id)
+    public static Singleton getInstance(int id) // we dont fo synchonize here as it will make every call to this method synchronized.
     {
         if(instance == null){
-        synchronized (Singleton.class) {
+        synchronized (Singleton.class) { // this will synchonize only this block which is creating instance.
             if (instance == null) {
                 instance = new Singleton(id);
             }
