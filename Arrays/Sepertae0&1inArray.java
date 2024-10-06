e6b3d44c7e88898e4e56b3af39c2cb7b00fa7b9d
yashbaijal <110350499+yashbaijal@users.noreply.github.com>
2024-10-06 20:23:48 +0530

    Create Sepertae0&1inArray.java

---
+++ Arrays/Sepertae0&1inArray.java
@@ -0,0 +1,43 @@
+public class Segregate  
+{ 
+    /*Function to put all 0s on left and all 1s on right*/
+    void segregate0and1(int arr[], int size)  
+    { 
+        
+        int left = 0, right = size - 1; 
+  
+        while (left < right)  
+        { 
+            /* Increment left index while we see 0 at left */
+            while (arr[left] == 0 && left < right) 
+               left++; 
+  
+            /* Decrement right index while we see 1 at right */
+            while (arr[right] == 1 && left < right) 
+                right--; 
+  
+            /* If left is smaller than right then there is a 1 at left 
+               and a 0 at right.  Exchange arr[left] and arr[right]*/
+            if (left < right)  
+            { 
+                arr[left] = 0; 
+                arr[right] = 1; 
+                left++; 
+                right--; 
+            } 
+        } 
+    } 
+    
+    public static void main(String[] args)  
+    { 
+        Segregate seg = new Segregate(); 
+        int arr[] = new int[]{0, 1, 0, 1, 1, 1}; 
+        int i, arr_size = arr.length; 
+  
+        seg.segregate0and1(arr, arr_size); 
+  
+        System.out.print("Array after segregation is "); 
+        for (i = 0; i < 6; i++) 
+            System.out.print(arr[i] + " "); 
+    } 
+} 
