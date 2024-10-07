5a78a30e86e20c0e816e18e4e829cecedfa247f9
yashbaijal <110350499+yashbaijal@users.noreply.github.com>
2024-10-07 14:49:25 +0530

    Update SubArraySum.java

--- Arrays/SubArraySum.java
+++ Arrays/SubArraySum.java
@@ -1,44 +1,73 @@
 class Codechef
 {
-    public static void subArraySum(int [] arr, int s)
-    {
-        int start=0;
-        int arrSum=0;
-        int end=0;
-        int flag=0;
-        
-        for(int i=0;i<arr.length;i++)
-        {
-            
-            while(arrSum>s && start<i-1)
-            {
-                arrSum=arrSum-arr[start];
-                start++;
-            }
-            
-            if(arrSum==s)
-            {
-                 end=i-1;
-                 flag=1;
-                 break;
-            }
-            
-            if(i<arr.length)
-            arrSum+=arr[i];
-        }
-        
-        if(flag==1)
-        {
-            System.out.println("subarray exists between :"+start+"and"+end);
-        }
-        else{
-             System.out.println("No subarray with given sum found");
-        }
-    }
+    public static void subarraySum(int[] arr,int sum)
+	{
+	       int l=0;
+	       int n=arr.length;
+	       int curr_sum=arr[0];
+	       
+	       for(int i=1;i<n;i++)
+	       {
+	              curr_sum=curr_sum+arr[i];
+	              while(curr_sum>sum && l<i)
+	              {
+	                     curr_sum=curr_sum-arr[l];
+	                     l++;
+	              }
+	              
+	              if(curr_sum==sum)
+	              {
+	                     System.out.println(l+" "+i);
+	                     return;
+	              }
+	              
+	       }
+	       System.out.println("No subarray");
+	}
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		int arr[] = { 15, 2, 4, 8, 9, 5, 10, 23 };
 		int sum=23;
 		subArraySum(arr,sum);
+	}
+}
+==============================================================================
+for -ve elements:
+class Codechef
+{
+	//Same program can be used to find Subarray with sum zero.
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		//subarray sum equal to k
+		int[] arr={10, 2, -2, -20, 10};
+		int sum=-10;
+		
+		subarraySum(arr,sum);
+		
+	}
+	
+	public static void subarraySum(int[] arr,int sum)
+	{
+	     HashMap<Integer, Integer>map=new HashMap<>();
+	     int curr_sum=0;
+	     for(int i=0;i<arr.length;i++)
+	     {
+	            curr_sum=curr_sum+arr[i];
+	            
+	            if(curr_sum==sum)
+	            {
+	                   System.out.println("Sub array found at indexs: "+0 +" "+i);
+	                   return;
+	            }
+	            if(map.containsKey(curr_sum-sum))
+	            {
+	                   System.out.println("Sub array found at indexs: "+(map.get(curr_sum-sum)+1)+" "+i);
+	                   return;
+	            }
+	            map.put(curr_sum,i);
+	     }
+	     
+	    System.out.println("No subarray found with given sum");
 	}
 }
